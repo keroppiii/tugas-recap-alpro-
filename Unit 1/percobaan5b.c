38dc48ba0b3ab93bbf5e4ccad8a9f88560b3b372
keroppiii <161756780+keroppiii@users.noreply.github.com>
2024-03-05 19:57:31 +0700

    Update percobaan5b.c

--- Unit 1/percobaan5b.c
+++ Unit 1/percobaan5b.c
@@ -8,3 +8,7 @@ int main(){
     printf("Halo %s", fullname);
     return 0;
 }
+
+//perbedaan output dari program pada percobaan5a dengan percobaan5b terdapat pada penggunaan fungsi scanf() dan fgets()
+//scanf() hanya dapat membaca satu buah kata pada teks 
+//sedangkan fgets() dapat membaca satu baris teks
