fea31eff0085793e69a284a2fa4563afb0d4f2ed
keroppiii <161756780+keroppiii@users.noreply.github.com>
2024-03-05 19:54:26 +0700

    Update percobaan2a.c

--- Unit 1/percobaan2a.c
+++ Unit 1/percobaan2a.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-
 int main() {
   printf("Alpro itu mudah!");
   return 0;
 }
+
+//kode tersebut error karena tidak menyertakan library "include <stdio.h>", sehingga program tidak mengerti kode yang ada
