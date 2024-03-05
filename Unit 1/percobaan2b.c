83683d0e5a8831f6ca12af7ea6a665c8f0ef6c25
keroppiii <161756780+keroppiii@users.noreply.github.com>
2024-03-05 19:54:58 +0700

    Update percobaan2b.c

--- Unit 1/percobaan2b.c
+++ Unit 1/percobaan2b.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  printf("Alpro itu mudah!");
+  printf(Alpro itu mudah!);
   return 0;
 }
+
+//kode tersebut error karena tidak terdapat tanda petik (" ") pada bagian tulisan yang ingin dicetak
